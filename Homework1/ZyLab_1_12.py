57eb314b5e8691874bac4f2e0e63beea73f796fd
LuisOnate <70404736+LuisOnate@users.noreply.github.com>
2020-09-11 15:01:00 -0500

    Add files via upload
    Zylab 1.12 lab commit.

---
+++ Homework1/ZyLab_1_12.py
@@ -0,0 +1,8 @@
+# Luis Onate
+# ID 1596900
+
+userNum = int(input())
+
+userNumSquared = userNum * userNum  # Bug here; fix it when instructed
+
+print(userNumSquared)  # Output formatting issue here; fix it when
