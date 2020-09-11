ad216c3ad28fd5268d587a9b1fc89de22c6bcf12
LuisOnate <70404736+LuisOnate@users.noreply.github.com>
2020-09-11 15:00:00 -0500

    Delete ZyLab_1_12.py

--- Homework1/ZyLab_1_12.py
+++
@@ -1,8 +0,0 @@
-# Luis Onate
-# ID 1596900
-
-userNum = int(input())
-
-userNumSquared = userNum * userNum  # Bug here; fix it when instructed
-
-print(userNumSquared)  # Output formatting issue here; fix it when
