3bb6062142e4b9384948ad0f0a55742d1f1baa63
LuisOnate <70404736+LuisOnate@users.noreply.github.com>
2020-09-11 16:09:52 -0500

    Add files via upload
    ZyLab 1.20 lab commit.

---
+++ Homework1/ZyLab_1_20.py
@@ -0,0 +1,23 @@
+# Luis Onate
+# ID 159600
+
+user_num1 = int(input('Enter integer:\n'))  # request user input integer
+
+squared_User1 = user_num1 * user_num1  # square function assigned to a variable
+
+cubed_user1 = user_num1 * user_num1 * user_num1  # cube function assigned to a variable
+
+# print input onto screen and squared solution along with cubed solution
+print('You entered:', user_num1)
+print(user_num1,'squared is', squared_User1)
+print('And', user_num1, 'cubed is', cubed_user1, '!!')
+
+user_num2 = int(input('Enter another integer:\n'))  # request second user input
+
+user2_sum = user_num1 + user_num2  # sum function assigned to a variable
+
+user2_product = user_num1 * user_num2  # multiplication function assigned to a variable
+
+# print sum and product solutions onto screen
+print(user_num1, '+', user_num2, 'is', user2_sum)
+print(user_num1, '*', user_num2, 'is', user2_product)
